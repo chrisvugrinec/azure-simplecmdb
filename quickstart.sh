3478b2488e977e6ca0e71ea9f2e178ed6e1b5e5f
gogsadmin <RedHat$1>
2018-06-17 15:33:59 +0200

    bfix

--- quickstart.sh
+++ quickstart.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Getting your cmdb app
-wget https://github.com/chrisvugrinec/azure-simplecmdb/blob/master/bin/cmdb-1.0.jar
+wget https://raw.github.com/chrisvugrinec/azure-simplecmdb/blob/master/bin/cmdb-1.0.jar
 # creating stuff you need
 mkdir -p ./scripts/results
 cd scripts
-wget https://github.com/chrisvugrinec/azure-simplecmdb/blob/master/scripts/createOverview.sh
+wget https://raw.githubusercontent.com/chrisvugrinec/azure-simplecmdb/master/scripts/createOverview.sh
+chmod 750 createOverview.sh
 echo "Now create your cmdb with the createOverview.sh script...files will be stored in results"
 echo "then open your jar....java -jar cmdb-1.0.jar"
